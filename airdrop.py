4c3f74aedef4cdd8cee9ae06dc1cf400d2d81353
Mark Essien <markessien@gmail.com>
2022-02-26 10:51:21 +0100

    Improved the airdrop code to save the ones it already sent to

--- airdrop.py
+++ airdrop.py
@@ -34,7 +34,19 @@ def airdrop(chia_exe, args):
         fee = 0.0000001
 
     target_address_list= set()
+    target_address_list_already_sent= set()
     try:
+
+        try:
+            with open(source_file + ".sent.csv", mode="r", encoding="utf-8") as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+        
+                for row in csv_reader:
+                    target_address_list_already_sent.add(row[0])
+        except:
+            pass
+        
+        print(str(len(target_address_list_already_sent)) + " addresses already sent")
 
         # Load all the addresses
         with open(source_file, mode="r", encoding="utf-8") as csv_file:
@@ -43,16 +55,29 @@ def airdrop(chia_exe, args):
             for row in csv_reader:
   
                 for column in row:
-                    if column.startswith("xch"):
+                    if column.startswith("xch") and not column in target_address_list_already_sent:
                         target_address_list.add(column)
                         break
 
-            print('Found ' + str(len(target_address_list)) + " unique XCH addresses")
+            print('Found ' + str(len(target_address_list)) + " unique XCH addresses for airdrop")
 
 
         # Start sending
         print("Starting Airdrop")
 
+        # Create a file where we write everything sent already so we can stop and start
+        # sending
+        try:
+            fsent = open(source_file + ".sent.csv", "w")
+
+            for sent_addr in target_address_list_already_sent:
+                fsent.write(sent_addr + ",\n")
+                fsent.flush()
+
+        except IOError:
+            print("Could not open file to write sent airdrops")
+            sys.exit()
+        
         for c, addr in enumerate(target_address_list):
             process = subprocess.run([chia_exe, "wallet", "send", 
                                                 "-i", str(wallet_id), 
@@ -62,9 +87,14 @@ def airdrop(chia_exe, args):
                                                 "-t", addr], stdout=subprocess.PIPE, universal_newlines=True)
             print(process.stdout)
             print("Airdropped to " + str(c+1))
+            fsent.write(addr + ",\n")
+            fsent.flush()
 
-            break
-            time.sleep(35)
+            if c == 5:
+                break
+            time.sleep(10)
+        
+        fsent.close()
 
     except IOError:
         print("Count not open the airdrop file. Please provide a valid CSV file with airdrop targets")
